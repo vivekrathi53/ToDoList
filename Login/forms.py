45c02b6e33d82d01d7e70342830a8bf185555266
vivekrathi53 <vivekrathi53@gmail.com>
2019-10-08 12:02:52 +0530

    Fixed Migrations

---
+++ Login/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import User
+class LoginForm(forms.ModelForm):
+	class Meta:
+		model = User
+		fields = [
+			'username',
+			'password',
+			'name',
+		]
--- Login/views.py
+++ Login/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from .forms import LoginForm
 # Create your views here.
 def login_page(request,*args,**kwargs):
-	return render(request,"Login.html",{})
+	form = LoginForm(request.POST or None)
+	if form.is_valid():
+		form.save()
+	context = {
+		"form": form
+	}
+	return render(request,"Login.html",context)
