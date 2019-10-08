5455d3c2d92c963c0a2c9009be64a9e42acffce2
vivekrathi53 <vivekrathi53@gmail.com>
2019-10-09 00:19:09 +0530

    Authentication Working but not reporting wrong username or password

--- Login/views.py
+++ Login/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .forms import LoginForm
 from .models import User
+from django.http import HttpRequest
 # Create your views here.
 def login_page(request,*args,**kwargs):
 	form = LoginForm(request.POST or None)
@@ -13,10 +14,11 @@ def login_page(request,*args,**kwargs):
 def authenticate_page(request,*args,**kwargs):
 	us = request.POST.get("username")
 	pas = request.POST.get("password")
-	obj = User.objects.get(username=us)
+	try:
+		obj = User.objects.get(username=us)
+	except:
+		return login_page(HttpRequest())
 	if pas == obj.password:
 		return render(request,"Home.html",{})
 	else:
-		return render(request,"Login.html",{})
-	
-
+		return login_page(HttpRequest())
