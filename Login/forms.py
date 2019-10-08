6471eaaa1c16f5e12a600ed6c84f5c5d8f0aaeac
vivekrathi53 <vivekrathi53@gmail.com>
2019-10-08 12:15:22 +0530

    login connected with database properly.

--- Login/forms.py
+++ Login/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from .models import User
 class LoginForm(forms.ModelForm):
+	username = forms.CharField(label='Username')
+	password = forms.CharField(widget=forms.PasswordInput)
 	class Meta:
 		model = User
 		fields = [
